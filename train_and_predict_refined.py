9b4388f55b2e21fb61f281a931770a6c048e8232
Gavin Li <lyo.gavin@gmail.com>
2018-04-30 00:34:45 +0800

    merge

--- train_and_predict_refined.py
+++ train_and_predict_refined.py
@@ -1659,7 +1659,7 @@ def use_config_scheme(str):
     return ret
 
 
-config_scheme_to_use = use_config_scheme('train_config_117_6')
+config_scheme_to_use = use_config_scheme('train_config_117_5')
 
 
 dtypes = {
