2874dd9c62f73a14fa89f45dd06996872d737ca7
Gavin Li <lyo.gavin@gmail.com>
2018-05-13 10:00:20 +0800

    fix bug in cumcount

--- train_utils/config_schema.py
+++ train_utils/config_schema.py
@@ -1544,4 +1544,4 @@ def use_config_scheme(str):
     return ret
 
 
-config_scheme_to_use = use_config_scheme('train_config_133_26')
+config_scheme_to_use = use_config_scheme('train_config_133_27')
