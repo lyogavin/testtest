08bd6ed50e155c07fbc0fbcfa73a93693e3d2c34
Gavin Li <lyo.gavin@gmail.com>
2018-05-17 18:49:08 +0800

    133 47~50 testing configs

--- run_themes.sh
+++ run_themes.sh
@@ -3,15 +3,26 @@ set -e
 
 
 
-echo "run train_config_133_45...."
+echo "run train_config_133_47...."
 
-python3 -u ./train_and_predict_simplified.py -c train_config_133_45
+python3 -u ./train_and_predict_simplified.py -c train_config_133_47
 
 
 
-echo "run train_config_133_46...."
+echo "run train_config_133_48...."
 
-python3 -u ./train_and_predict_simplified.py -c train_config_133_46
+python3 -u ./train_and_predict_simplified.py -c train_config_133_48
+
+
+echo "run train_config_133_49...."
+
+python3 -u ./train_and_predict_simplified.py -c train_config_133_49
+
+
+
+echo "run train_config_133_50...."
+
+python3 -u ./train_and_predict_simplified.py -c train_config_133_50
 
 
 echo 'done'
