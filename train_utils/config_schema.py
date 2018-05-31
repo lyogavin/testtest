a55b347c2448eca6fc16c9b3a7b4f6f76d31b519
Gavin Li <lyo.gavin@gmail.com>
2018-05-31 20:37:55 +0800

    133 87

--- train_utils/config_schema.py
+++ train_utils/config_schema.py
@@ -1917,6 +1917,25 @@ train_config_133_85.add_features_list = add_features_list_origin_no_channel_next
 train_config_133_86 = copy.deepcopy(train_config_133_66)
 train_config_133_86.add_features_list = train_config_133_86.add_features_list + get_cols_com('var')
 
+train_config_133_87 = copy.deepcopy(train_config_133_86)
+train_config_133_87.lgbm_params = [
+    lgbm_params_pub_entire_set_new_test_1,
+    lgbm_params_pub_entire_set_new_test_2,
+    lgbm_params_pub_entire_set_new_test_3,
+    lgbm_params_pub_entire_set_new_test_4,
+    lgbm_params_pub_entire_set_new_test_5,
+    lgbm_params_pub_entire_set_new_test_6,
+    lgbm_params_pub_entire_set_new_test_7,
+    lgbm_params_pub_entire_set_new_test_8,
+    lgbm_params_pub_entire_set_test_early_stop_50,
+    lgbm_params_pub_entire_set_test_early_stop_200,
+    lgbm_params_pub_entire_set_test_depth_5,
+    lgbm_params_pub_entire_set_test_depth_5_leave_9,
+    lgbm_params_pub_entire_set_test_scale_pos_50,
+    lgbm_params_pub_entire_set_test_scale_pos_90,
+    lgbm_params_pub_entire_set_test_early_stop_400,
+]
+
 
 
 debug = False
