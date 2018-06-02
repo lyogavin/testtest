376cb16c624ecd5ed328c40522bb51c198265a57
Gavin Li <lyo.gavin@gmail.com>
2018-06-02 14:26:39 +0800

    133 89 90

--- train_utils/model_params.py
+++ train_utils/model_params.py
@@ -379,3 +379,8 @@ lgbm_params_pub_entire_set_test_depth_5_leave_20_scale_1.update({'max_depth': 5,
 lgbm_params_pub_entire_set_test_depth_5_leave_30_scale_1 = dict(new_lgbm_params)
 lgbm_params_pub_entire_set_test_depth_5_leave_30_scale_1.update({'max_depth': 5, 'num_leaves': 30, 'scale_pos_weight':1.0})
 
+lgbm_params_pub_entire_set_test_depth_5_leave_25_scale_1 = dict(new_lgbm_params)
+lgbm_params_pub_entire_set_test_depth_5_leave_25_scale_1.update({'max_depth': 5, 'num_leaves': 25, 'scale_pos_weight':1.0})
+
+lgbm_params_pub_entire_set_test_depth_4_leave_30_scale_1 = dict(new_lgbm_params)
+lgbm_params_pub_entire_set_test_depth_4_leave_30_scale_1.update({'max_depth': 4, 'num_leaves': 30, 'scale_pos_weight':1.0})
