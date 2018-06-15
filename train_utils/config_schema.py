3d79d37bdfcfdb1587fb16f5ce2822d5c6180424
Gavin Li <lyo.gavin@gmail.com>
2018-06-15 10:17:03 +0800

    104 test furhter overfitting prevention

--- train_utils/config_schema.py
+++ train_utils/config_schema.py
@@ -2204,6 +2204,33 @@ train_config_133_103.lgbm_params = [{ **lgbm_params_pub_entire_set_test_depth_4_
                                     ]
 
 
+train_config_133_104 = copy.deepcopy(train_config_133_97)
+train_config_133_104.use_ft_cache = True
+train_config_133_104.lgbm_params = [{ **lgbm_params_pub_entire_set_test_depth_4_leave_20_scale_1,
+                                     **{'early_stopping_round': 300,
+                                        'colsample_bytree': 0.5,
+                                        'subsample': 0.9,
+                                        'reg_alpha': 0.2,
+                                        'max_bin': 150,
+                                        'min_child_samples': 1000,
+                                        }},
+                                     { **lgbm_params_pub_entire_set_test_depth_4_leave_20_scale_1,
+                                     **{'early_stopping_round': 300,
+                                        'colsample_bytree': 0.5,
+                                        'subsample': 0.9,
+                                        'reg_alpha': 0.3,
+                                        'max_bin': 150,
+                                        'min_child_samples': 1000,
+                                        }},
+                                     { **lgbm_params_pub_entire_set_test_depth_4_leave_20_scale_1,
+                                     **{'early_stopping_round': 300,
+                                        'colsample_bytree': 0.5,
+                                        'subsample': 0.9,
+                                        'reg_alpha': 0.2,
+                                        'max_bin': 150,
+                                        'min_child_samples': 1500,
+                                        }},
+                                    ]
 debug = False
 
 def use_config_scheme(str):
