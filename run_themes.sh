e5b041e37942b668eec1c588d71c8814e4b8101d
Gavin Li <lyo.gavin@gmail.com>
2018-08-01 08:41:30 +0800

    copy last min online kernel 160

--- run_themes.sh
+++ run_themes.sh
@@ -5,12 +5,12 @@ set -e
 #echo 136
 #echo "run train_config_133_136...."
 #python3 -u ./train_and_predict_simplified.py -c train_config_133_136
-echo 141
-echo "run train_config_133_141...."
-python3 -u ./train_and_predict_simplified.py -c train_config_133_141
-echo 143
-echo "run train_config_133_143...."
-python3 -u ./train_and_predict_simplified.py -c train_config_133_143
+#echo 141
+#echo "run train_config_133_141...."
+#python3 -u ./train_and_predict_simplified.py -c train_config_133_141
+#echo 143
+#echo "run train_config_133_143...."
+#python3 -u ./train_and_predict_simplified.py -c train_config_133_143
 
 for i in `seq 160 160`;
 do
